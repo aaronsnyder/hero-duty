b74b14ba4152b4f68d989536cc882a09126d4920
Aaron Snyder <a453803@gmail.com>
2016-02-04 22:18:13 -0700

    Cleaning up schedule_generator.rb to utilize Settings. Also, adding a test to make sure it's re-generating as expected.

--- app/helpers/schedule_generator.rb
+++ app/helpers/schedule_generator.rb
@@ -2,20 +2,41 @@ require 'singleton'
 
 class ScheduleGenerator
   include Singleton
-  
-  def reGenerateSchedule(*suggested_order)
-    connection = ActiveRecord::Base.connection
-    if suggested_order == nil
+
+  def reGenerateSchedule(days_to_schedule=30,suggested_order=[],skip_weekends=false,skip_holidays=false)
+    db_connection = ActiveRecord::Base.connection
+    todays_date = Date.today
+    
+    # TODO: 
+    # make this work by days_to_schedle, instead of just iterating suggested order
+    # make the suggested_order array the same length as the days_to_schedule
+    # as an alternative, just pull distinct names out of the DB, and use that to build the array
+    # account for weekends
+    # account for holidays
+    # remove the BS array from this file...
+    
+    if suggested_order.length == 0
       suggested_order = ['Sherry', 'Boris', 'Vicente', 'Matte', 'Jack', 'Sherry','Matte', 'Kevin', 'Kevin', 'Vicente', 'Zoe', 'Kevin','Matte', 'Zoe', 'Jay', 'Boris', 'Eadon', 'Sherry','Franky', 'Sherry', 'Matte', 'Franky', 'Franky', 'Kevin','Boris', 'Franky', 'Vicente', 'Luis', 'Eadon', 'Boris','Kevin', 'Matte', 'Jay', 'James', 'Kevin', 'Sherry','Sherry', 'Jack', 'Sherry', 'Jack'
       ]
     end
+    
     # remove current schedules
     Shift.destroy_all
-    # get the suggested order
     
-    # compare to unavailable days
-    
-    # create new schedules
-    
+    suggested_order.each_with_index do |name,i|
+      date = todays_date+i
+      employee = Employee.find_by first_name: name
+      
+      if !employee
+        # we didn't find this employee...moving on!
+        continue
+      end
+      
+      if !employee.work_restrictions.find_by date: date
+        # No work restrictions, today!
+        employee.shifts.create({on_call_date: Date.today})
+      end
+    end
+    # create new schedules    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205040536) do
+ActiveRecord::Schema.define(version: 20160205042257) do
 
   create_table "employees", force: true do |t|
     t.string   "first_name"
