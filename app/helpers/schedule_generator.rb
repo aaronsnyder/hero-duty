eb484b5dc187fb89afe37f8d5b15ea4e0a07a99b
Aaron Snyder <snyder.aaronp@gmail.com>
2016-02-05 10:44:49 -0700

    trying to get test to pass...

--- app/helpers/schedule_generator.rb
+++ app/helpers/schedule_generator.rb
@@ -3,38 +3,47 @@ require 'singleton'
 class ScheduleGenerator
   include Singleton
 
-  def reGenerateSchedule(days_to_schedule=30,suggested_order=[],skip_weekends=false,skip_holidays=false)
+  def reGenerateSchedule(days_to_schedule=30,suggested_order=[],skip_weekends=true,skip_holidays=true)
     db_connection = ActiveRecord::Base.connection
     todays_date = Date.today
     
-    # TODO: 
-    # make this work by days_to_schedle, instead of just iterating suggested order
-    # make the suggested_order array the same length as the days_to_schedule
+    # TODO:
     # as an alternative, just pull distinct names out of the DB, and use that to build the array
-    # account for weekends
-    # account for holidays
-    # remove the BS array from this file...
+    # build some tests
     
-    if suggested_order.length == 0
-      suggested_order = ['Sherry', 'Boris', 'Vicente', 'Matte', 'Jack', 'Sherry','Matte', 'Kevin', 'Kevin', 'Vicente', 'Zoe', 'Kevin','Matte', 'Zoe', 'Jay', 'Boris', 'Eadon', 'Sherry','Franky', 'Sherry', 'Matte', 'Franky', 'Franky', 'Kevin','Boris', 'Franky', 'Vicente', 'Luis', 'Eadon', 'Boris','Kevin', 'Matte', 'Jay', 'James', 'Kevin', 'Sherry','Sherry', 'Jack', 'Sherry', 'Jack'
-      ]
-    end
+    # Grab the suggested order from yaml config file
+    suggested_order = Settings.predefined_shift_order
     
     # remove current schedules
     Shift.destroy_all
     
-    suggested_order.each_with_index do |name,i|
+    employee_counter = 0
+    (0..days_to_schedule).each do |i|
       date = todays_date+i
-      employee = Employee.find_by first_name: name
       
+      if skip_weekends and (date.saturday? or date.sunday?)
+        next
+      end
+      
+      if skip_holidays and Holidays.on(date, :us).length > 0
+        next
+      end
+      
+      employee = Employee.find_by first_name: suggested_order[employee_counter]
       if !employee
         # we didn't find this employee...moving on!
-        continue
+        next
       end
       
       if !employee.work_restrictions.find_by date: date
         # No work restrictions, today!
-        employee.shifts.create({on_call_date: Date.today})
+        employee.shifts.create({on_call_date: date})
+        if employee_counter == suggested_order.length
+          #start back at the beginning
+          employee_counter = 0
+        else
+          employee_counter += 1
+        end
       end
     end
     # create new schedules    
