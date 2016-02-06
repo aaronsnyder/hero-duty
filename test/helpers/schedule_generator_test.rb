1794c6510c61c68f7bb62c78bd0b21a7fd38382e
Aaron Snyder <a453803@gmail.com>
2016-02-05 20:21:21 -0700

    Cleaning up schedule_generator.rb to utilize Settings. Also, adding a test to make sure it's re-generating as expected.

---
+++ test/helpers/schedule_generator_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ScheduleGeneratorTest < ActiveSupport::TestCase
+  test "Ensure the schedule generator...does" do 
+    ScheduleGenerator.instance.reGenerateSchedule
+    
+    shift_count = Shift.count()
+    assert shift_count > 0, 'Shift count == 0'
+  end
+end
