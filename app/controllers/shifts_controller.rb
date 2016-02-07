644aead83871203b7e8c60144173dd8127de985b
Aaron Snyder <a453803@gmail.com>
2016-02-07 07:41:47 -0700

    removed the last of the n+1 queries

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsController < ApplicationController
   def index
     # including :employee to remove n+1 possibility...
-    @shifts = Shift.includes(:employee).where("on_call_date > ?", Date.today).where("on_call_date <= ?", Date.today + 1.month)
+    @shifts = Shift.includes(:employee,:employee => :work_restrictions).where("on_call_date > ?", Date.today).where("on_call_date <= ?", Date.today + 1.month)
     @current_shift = Shift.find_by on_call_date: Date.today
     
     if @shifts.last.on_call_date < Date.today.beginning_of_month.next_month
@@ -12,7 +12,7 @@ class ShiftsController < ApplicationController
   
   def show    
     # including :employee to remove n+1 possibility...
-    @shifts = Shift.includes(:employee).where("on_call_date >= ?", Date.today).where("on_call_date <= ?", Date.today + 1.month).where(:employees => {:first_name => params[:first_name]})
+    @shifts = Shift.includes(:employee,:employee => :work_restrictions).where("on_call_date >= ?", Date.today).where("on_call_date <= ?", Date.today + 1.month).where(:employees => {:first_name => params[:first_name]})
     @hero = params[:first_name]
   end
   
